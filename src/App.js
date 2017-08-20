4e5acbbe4e637f08536ea9e6d39d9d21dd8aaeef
wlu7028 <wlu7028@gmail.com>
2017-08-19 22:43:58 -0700

    load book list from external data

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import './App.css'
 import SearchBook from './SearchBook'
 import ListBooks from './ListBooks'
 
+
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
+    books: []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+      console.log(books)
+    })
+  }
   render() {
     return (
       <div className="app">
@@ -28,7 +29,7 @@ class BooksApp extends React.Component {
 
         <Route exact path='/' render={({ history }) => ( 
           <ListBooks
-
+            books={this.state.books}
           />
         )}/>
       </div>
